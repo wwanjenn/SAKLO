1c9a05be137ba9826426ac13b287ff6c5ca779f4
JLyndon <jolyndonjr@gmail.com>
2023-06-21 13:38:32 +0800

    Initial implementation of firebase to app

--- app/src/main/java/com/codegrace/Saklo/fragment/loginRegister/RegisterFragment.kt
+++ app/src/main/java/com/codegrace/Saklo/fragment/loginRegister/RegisterFragment.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.codegrace.Saklo.R
 import com.codegrace.Saklo.databinding.FragmentRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterFragment: Fragment(R.layout.fragment_register) {
     private lateinit var binding: FragmentRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,8 +27,22 @@ class RegisterFragment: Fragment(R.layout.fragment_register) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
         binding.btnRegister.setOnClickListener {
-            findNavController().navigate(R.id.action_registerFragment_to_mainActivity)
+            val email = binding.regEmail.text.toString()
+            val password = binding.regPassword.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()){
+                firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
+                    if(it.isSuccessful){
+                        Toast.makeText(requireActivity(), "Successfully Registered!", Toast.LENGTH_SHORT).show()
+                        findNavController().navigate(R.id.action_registerFragment_to_mainActivity)
+                    }else{
+                        Toast.makeText(requireActivity(), it.exception.toString(), Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
         }
 
         binding.btnTologin.setOnClickListener {
