2b505e220659f68cc1a4bcaf14b7b9f4f37df1cf
JLyndon <jolyndonjr@gmail.com>
2023-06-22 00:00:03 +0800

    Added function: Close app on onBackPressed in Main if user is logged in

--- app/src/main/java/com/codegrace/Saklo/activities/MainActivity.kt
+++ app/src/main/java/com/codegrace/Saklo/activities/MainActivity.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.app.ActivityCompat.finishAffinity
 import com.codegrace.Saklo.R
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var firebaseAuth:FirebaseAuth
+    private var firebaseAuth: FirebaseAuth? = null
+    var mAuthListener: FirebaseAuth.AuthStateListener? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,9 +36,35 @@ class MainActivity : AppCompatActivity() {
 
         val btnLogout = findViewById<Button>(R.id.btnSignout)
         btnLogout.setOnClickListener {
-            firebaseAuth.signOut();
+            firebaseAuth!!.signOut()
             val intentLogout = Intent(this, RegisterLoginActivity::class.java)
             startActivity(intentLogout)
+        }
+
+        mAuthListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user = firebaseAuth.currentUser
+        }
+
+        firebaseAuth?.addAuthStateListener(mAuthListener!!)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        firebaseAuth?.removeAuthStateListener(mAuthListener!!)
+    }
+
+    override fun onBackPressed() {
+        val user = firebaseAuth?.currentUser
+        handleBackPressed(user)
+    }
+
+    private fun handleBackPressed(user: FirebaseUser?) {
+        if (user != null) {
+            finishAffinity()
+            finish()
+        } else {
+            startActivity(Intent(this@MainActivity, RegisterLoginActivity::class.java))
+            finish()
         }
     }
 }
