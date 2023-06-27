17de3b2b70db7f2e54f170cbfecd7feaf5c9b7fb
arru <romalynfloranda0203@gmail.com>
2023-06-27 22:11:03 +0800

    appointment screen but still not finished

--- app/src/main/java/com/codegrace/Saklo/activities/AppointmentActivity.kt
+++ app/src/main/java/com/codegrace/Saklo/activities/AppointmentActivity.kt
@@ -2,6 +2,9 @@ package com.codegrace.Saklo.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.codegrace.Saklo.R
 
 class AppointmentActivity : AppCompatActivity() {
@@ -9,5 +12,15 @@ class AppointmentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_appointment)
 
+    }
+
+    private fun changeStatusBarTextColor() {
+        val decorView: View = window.decorView
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        WindowInsetsControllerCompat(window, decorView).run {
+            isAppearanceLightStatusBars = true
+            isAppearanceLightNavigationBars = true
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
     }
 }
