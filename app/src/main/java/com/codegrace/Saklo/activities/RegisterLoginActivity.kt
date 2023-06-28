da26f0f93c3dec517a56d5a1ff7cd69636b5648c
JLyndon <jolyndonjr@gmail.com>
2023-06-28 15:10:48 +0800

    Reverted onBackPress func; Added vector art in design

--- app/src/main/java/com/codegrace/Saklo/activities/RegisterLoginActivity.kt
+++ app/src/main/java/com/codegrace/Saklo/activities/RegisterLoginActivity.kt
@@ -5,40 +5,27 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
-import androidx.activity.OnBackPressedCallback
-import androidx.fragment.app.FragmentManager
 import com.codegrace.Saklo.R
 
 class RegisterLoginActivity : AppCompatActivity() {
-    private var doubleBackToExitPressedOnce = false
+    var doubleBackToExitPressedOnce = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_login)
+
     }
-
     override fun onBackPressed() {
-        val manager: FragmentManager = supportFragmentManager
-        val backStackEntryCount = manager.backStackEntryCount
-
-        // Check if fragments are in the starting screen
-        if (backStackEntryCount == 0) {
-            if (doubleBackToExitPressedOnce) {
-                finish()
-            } else {
-                doubleBackToExitPressedOnce = true
-                Toast.makeText(
-                    applicationContext,
-                    "Please click back again to exit",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-                Handler(Looper.getMainLooper()).postDelayed({
-                    doubleBackToExitPressedOnce = false
-                }, 2000)
-            }
-        } else {
+        if (doubleBackToExitPressedOnce) {
             super.onBackPressed()
+            return
         }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            doubleBackToExitPressedOnce = false
+        }, 5000)
     }
 }
