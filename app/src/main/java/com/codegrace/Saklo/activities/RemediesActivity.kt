bcabd766c6a7646478e5a36fe9b5e241426a7f60
arru <romalynfloranda0203@gmail.com>
2023-07-03 23:07:26 +0800

    remedies part fixed

--- app/src/main/java/com/codegrace/Saklo/activities/RemediesActivity.kt
+++ app/src/main/java/com/codegrace/Saklo/activities/RemediesActivity.kt
@@ -3,6 +3,9 @@ package com.codegrace.Saklo.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.codegrace.Saklo.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -11,6 +14,7 @@ class RemediesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_remedies)
+        changeStatusBarTextColor()
 
         bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
         bottomNav.setOnItemSelectedListener {
@@ -30,6 +34,16 @@ class RemediesActivity : AppCompatActivity() {
 
                 else -> throw AssertionError()
             }
+        }
+    }
+
+    private fun changeStatusBarTextColor() {
+        val decorView: View = window.decorView
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        WindowInsetsControllerCompat(window, decorView).run {
+            isAppearanceLightStatusBars = true
+            isAppearanceLightNavigationBars = true
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
     }
 }
