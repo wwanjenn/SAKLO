3706ebdf3fb9b82a8fd4d5d23ea8ad9316f91c8b
Lemonade7 <wmaglangit1118@gmail.com>
2023-07-05 00:17:53 +0800

    made remedies entity data class

--- app/src/main/java/com/codegrace/Saklo/Remedies.kt
+++ app/src/main/java/com/codegrace/Saklo/Remedies.kt
@@ -1,3 +1,15 @@
 package com.codegrace.Saklo
 
-data class Remedies()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.sql.Blob
+
+@Entity
+data class Remedies(
+    val nameCommon: String,
+    val nameScientific: String,
+    val detailPara: String,
+    val pic: Blob,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+)
