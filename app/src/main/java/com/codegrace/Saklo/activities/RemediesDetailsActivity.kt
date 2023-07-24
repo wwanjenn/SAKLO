c738cd7c28b448d4ec07988db88926ac531e084a
arru <romalynfloranda0203@gmail.com>
2023-07-24 23:01:38 +0800

    fe fixes (drugs and remedies details layouts,darkmode,margins,text sizes)

--- app/src/main/java/com/codegrace/Saklo/activities/RemediesDetailsActivity.kt
+++ app/src/main/java/com/codegrace/Saklo/activities/RemediesDetailsActivity.kt
@@ -1,14 +1,21 @@
 package com.codegrace.Saklo.activities
 
+import android.graphics.text.LineBreaker.JUSTIFICATION_MODE_INTER_WORD
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.codegrace.Saklo.R
 
 class RemediesDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_remedies_details)
+
+        changeStatusBarTextColor()
 
         val nameCommon: String = intent.getStringExtra("nameCommon").toString()
         val nameScientific: String = intent.getStringExtra("nameScientific").toString()
@@ -25,6 +32,15 @@ class RemediesDetailsActivity : AppCompatActivity() {
         detailsParaTV.text = detailsPara
         warningsParaTV.text = warningsPara
 
-
     }
+
+    private fun changeStatusBarTextColor() {
+
+        val decorView: View = window.decorView
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        val windowInsetsController = WindowInsetsControllerCompat(window, decorView)
+        windowInsetsController.isAppearanceLightStatusBars =
+            resources.configuration.uiMode and android.content.res.Configuration.UI_MODE_NIGHT_MASK != android.content.res.Configuration.UI_MODE_NIGHT_YES
+    }
+
 }
